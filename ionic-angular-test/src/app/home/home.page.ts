60ce6372f15c956a9140cadd5a447141248c6444
Johan Perez <nasa_science@hotmail.com>
2020-09-05 01:42:05 +0200

    first app changes - adding button and changing text

--- ionic-angular-test/src/app/home/home.page.ts
+++ ionic-angular-test/src/app/home/home.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit{
 
   constructor() {}
+
+    text:string = ""
+
+    ngOnInit(){
+  
+    }
+  
+    showMessage(){
+      this.text = "Great, you have done something new."
+    }
 
 }
